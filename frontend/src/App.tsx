af2d100ef3547b0781792dc875a7448a7b43da5a
mcortezv <mdjesuscv@gmail.com>
2025-12-05 00:41:48 -0700

    Panel de Administrador

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import Hero from "./pages/Hero";
+import Chat from "./pages/Chat";
 import Config from "./pages/Server";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -7,6 +8,7 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Hero />} />
+        <Route path="/chat" element={<Chat />} />
         <Route path="/server" element={<Config />} />
       </Routes>
     </BrowserRouter>
--- frontend/src/componets/ComboBox.tsx
+++ frontend/src/componets/ComboBox.tsx
@@ -2,11 +2,6 @@ import { Select, Option } from "@material-tailwind/react";
 
 export function ComboBox() {
     return (
-        <div className="w-50">
-            <Select label="Protocolo">
-                <Option className="text-left">TCP</Option>
-                <Option className="text-left">UDP</Option>
-            </Select>
-        </div>
+        
     );
 }
