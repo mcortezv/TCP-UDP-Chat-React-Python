a00b9d1ccc34d75e4e74eb595eded96e3522be1b
mcortezv <mdjesuscv@gmail.com>
2026-04-14 08:41:06 -0700

    update: login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,22 @@
 import Hero from "./pages/Hero";
 import Chat from "./pages/Chat";
 import Server from "./pages/Server";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+function RequireAuth({ children }: { children: JSX.Element }) {
+  const username = sessionStorage.getItem("username");
+  return username ? children : <Navigate to="/login" replace />;
+}
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Hero />} />
-        <Route path="/chat" element={<Chat />} />
-        <Route path="/server" element={<Server />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<RequireAuth><Hero /></RequireAuth>} />
+        <Route path="/chat" element={<RequireAuth><Chat /></RequireAuth>} />
+        <Route path="/server" element={<RequireAuth><Server /></RequireAuth>} />
       </Routes>
     </BrowserRouter>
   );
